f115bc145430e527107b92a8f0d12861ef7a4f37
Dima-Gri <60757623+Dima-Gri@users.noreply.github.com>
2022-06-26 13:39:43 +0300

    Create topology_sort.cpp

---
+++ Topology sort/topology_sort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include "vector"
+
+std::vector<size_t> topology;
+bool is_exist = false;
+
+enum class Flag {
+    Unused,
+    Process,
+    Used
+};
+
+void dfs(size_t now, size_t parent, std::vector<std::vector<int>> &graph, std::vector<Flag> &used) {
+    used[now] = Flag::Process;
+    for (const auto &neigh: graph[now]) {
+        if (used[neigh] == Flag::Process && neigh != parent) {
+            is_exist = true;
+        } else if (used[neigh] == Flag::Unused) {
+            dfs(neigh, now, graph, used);
+        }
+    }
+    used[now] = Flag::Used;
+    topology.push_back(now);
+}
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    std::vector<std::vector<int>> graph(n + 1);
+    std::vector<Flag> used(n + 1, Flag::Unused);
+
+    for (size_t i = 0; i < m; ++i) {
+        int a, b;
+        std::cin >> a >> b;
+        graph[a].push_back(b);
+    }
+
+    for (size_t i = 1; i <= n; ++i) {
+        if (used[i] == Flag::Unused) {
+            dfs(i, 0, graph, used);
+        }
+    }
+
+    std::reverse(topology.begin(), topology.end());
+
+    if (!is_exist) {
+        for (const auto &it: topology) {
+            std::cout << it << " ";
+        }
+        return 0;
+    }
+    std::cout << "Topology sort can not be build";
+
+    return 0;
+}
